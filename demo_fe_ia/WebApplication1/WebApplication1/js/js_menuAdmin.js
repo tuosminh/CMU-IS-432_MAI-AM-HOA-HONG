facb431d84386ad87e16ce5cc95debfd0c126cb7
Lyly43 <nguyenly04032004@gmail.com>
2024-11-21 22:19:09 +0700

    master admin

---
+++ demo_fe_ia/WebApplication1/WebApplication1/js/js_menuAdmin.js
@@ -0,0 +1,39 @@
+﻿const body = document.querySelector("body"),
+    sidebar = body.querySelector(".sidebar"),
+    toggle = body.querySelector(".toggle"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+sunIcon = body.querySelector(".sun"),
+    moonIcon = body.querySelector(".moon");
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+});
+
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("light");
+
+    if (body.classList.contains("light")) {
+        modeText.innerText = "Dark mode"
+        sunIcon.style.opacity = "0"; // Ẩn icon Sun
+        moonIcon.style.opacity = "1"; // Hiện icon Moon
+    }
+    else {
+        modeText.innerText = "Light mode"
+        sunIcon.style.opacity = "1"; // Hiện icon Sun
+        moonIcon.style.opacity = "0"; // Ẩn icon Moon
+    }
+});
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const body = document.querySelector("body");
+//     const modeSwitch = document.querySelector(".toggle-switch");
+
+//     // Toggle dark mode
+//     modeSwitch.addEventListener("click", () => {
+//         body.classList.toggle("light");
+//     });
+// });
+
